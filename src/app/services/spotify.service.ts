70d9803810071fe83a545956e2914e1a5c62eef5
David Guzman <davecalix@gmail.com>
2020-11-17 21:36:26 -0600

    Cambios para obtener el token como metodo

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,23 +1,38 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpotifyService {
+  token: string;
+  body = {
+    grant_type: 'client_credentials',
+    client_id: '9f7996d6373e4700abaff38928b8abe9',
+    client_secret: 'e0e8ac0c946f48579d0b895a020110de',
+  };
   constructor(private http: HttpClient) {
     console.log('Spotify service listo');
   }
 
   getNewReleases(): void {
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBy4YtUfnPAiIh58s9zveePU-LVTf6Vyjdb8hG_17XskYRQiRjzGsxYOzDXFZGClxDr0mALDYfeZ31WZC4',
+      Authorization: this.getNewToken(),
     });
     this.http
       .get('https://api.spotify.com/v1/browse/new-releases', { headers })
       .subscribe((releases) => {
         console.log(releases);
       });
+  }
+
+  getNewToken(): string {
+    this.http
+    .post('https://accounts.spotify.com/api/token', this.body)
+    .subscribe(response => {
+      console.log(response);
+    });
+    return this.token;
   }
 }
